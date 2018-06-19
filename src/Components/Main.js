849a01a020016d83bc2622b2057dadefa2f5cf90
isaacmabueno <isaacmabueno@gmail.com>
2018-06-19 10:25:46 -0500

    updating styles and removing excess js files

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,26 +1,15 @@
 import React, {Component} from 'react'
 import Title from './Title'
 import SubTitle from './SubTitle'
-import List from './List'
+// import List from './List'
 
 class Main extends Component {
     render() {
         return(
             <div>
-                <Title todo = {'toDos'} />
+                <Title todo = {'React Photo'} />
                 <SubTitle/>
-                <List tasks =   {[
-                                    'Pack bags to head back to Chicago',
-                                    'download podcasts for the drive home',
-                                    'Make sure we arent leaving baby stuff behind'
-                                ]}/>
-
-                <List tasks =   {[
-                                    'Go to breakfast with family',
-                                    'Drive home to from MI',
-                                    'Watch the NBA Finals'
-                                ]}/>
-            </div>
+                            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main'
+import './styles/styles.css'
 
 
 
