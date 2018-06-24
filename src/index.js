c80d4f6a8b1a831215eda588d2244349d45b2b84
isaacmabueno <isaacmabueno@gmail.com>
2018-06-23 23:19:12 -0500

    configuring via props

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 // import Main from './Components/Main'
-// import './styles/styles.css'
-
-const tasks = ['Design TPD YC Booklet', 'Practice React', 'Slsdfeep']
+import './styles/styles.css'
 
     class List extends Component {
         render() {
             return( <ol>
-                {tasks.map((task, index)=> <li key={index}> {task} </li> )}
+                {this.props.tasks.map((task, index)=> <li key={index}> {task} </li> )}
             </ol>)
         }
     }
 
     class Title extends Component {
         render() {
-            return <h1>Task List </h1>
+            return <h1>{this.props.title} </h1>
         }
     }
 
     class Main extends Component {
         render() {
-            return <div><Title/><List/></div>
+            return (
+            <div>
+                <Title title={'React Photo'}/>
+                <List tasks={['do design mockup for TPD', 'finish the laundry']}/>
+                <List tasks={['Put the baby down for bed', 'walk the dog']}/>
+            </div>
+        );
         }
     }
 
