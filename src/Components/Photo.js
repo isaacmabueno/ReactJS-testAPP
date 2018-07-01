5fc61ec61b35b10b17558920d2230251ab0439b1
isaacmabueno <isaacmabueno@gmail.com>
2018-06-30 21:26:12 -0500

    refactoring code. creating functional based components and only leaving class components on main.js

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react'
+import Reactfrom 'react'
 
-class Photo extends Component {
-    render() {
-        const post = this.props.post
-        return <figure className="figure">
-            <img className="photo" src={post.imageLink} alt={post.description} />
-            <figcaption><p>{post.description}</p></figcaption>
-            <div className="button-container"></div>
-            <button className="remove-button">Remove</button>
-        </figure>
-    }
+function Photo(props) {
+    const post = props.post
+    return <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption><p>{post.description}</p></figcaption>
+        <div className="button-container"></div>
+        <button className="remove-button">Remove</button>
+    </figure>
 }
 
 export default Photo
