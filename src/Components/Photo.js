92fa08a6ba000ef0f51cf7a1da2a652e70873396
isaacmabueno <isaacmabueno@gmail.com>
2018-07-30 21:10:47 -0500

    adding more prop types

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Photo(props) {
     const post = props.post
@@ -10,6 +11,11 @@ function Photo(props) {
             props.onRemovePhoto(post)
         }}>Remove</button>
     </figure>
+}
+
+Photo.propTypes = {
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default Photo
