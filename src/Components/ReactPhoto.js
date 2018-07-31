09cfbf8aa81ac2916b429d9425dcfdd5f0fe5fa6
isaacmabueno <isaacmabueno@gmail.com>
2018-07-30 21:06:08 -0500

    adding prop types

--- src/Components/ReactPhoto.js
+++ src/Components/ReactPhoto.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Photo from './Photo'
+import PropTypes from 'prop-types'
 
 function ReactPhoto(props) {
     return <div className="photoGrid">
         {props.posts.map((post, index) => <Photo key={index} post = {post} onRemovePhoto={props.onRemovePhoto}/>)}
     </div>
+}
+
+ReactPhoto.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+
 }
 
 export default ReactPhoto
